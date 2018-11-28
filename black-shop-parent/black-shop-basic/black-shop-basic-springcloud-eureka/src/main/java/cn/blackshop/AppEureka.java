a6ac6cb84df65e8e379bb171489cc91ecdc4c42f
lizibin <125720240@qq.com>
2018-11-28 20:34:45 +0800

    提交Eureka配置和apollo的配置

---
+++ black-shop-parent/black-shop-basic/black-shop-basic-springcloud-eureka/src/main/java/cn/blackshop/AppEureka.java
@@ -0,0 +1,32 @@
+/**  
+ 
+* <p>Company: www.black-shop.cn</p>  
+
+* <p>Copyright: Copyright (c) 2018</p>   
+
+* @version 1.0  
+
+* black-shop(黑店) 版权所有,并保留所有权利。
+
+*/  
+package cn.blackshop;
+
+import org.springframework.boot.SpringApplication;
+
+/**  
+
+* <p>Title: AppEureka</p>  
+
+* <p>Description: </p>  
+
+* @author zibin  
+
+* @date 2018年11月28日  
+
+*/
+public class AppEureka {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEureka.class, args);
+	}
+}
