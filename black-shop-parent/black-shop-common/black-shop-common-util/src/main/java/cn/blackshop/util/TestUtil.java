29ae60f66d88f82da89dbd8a3f096534f8147c71
lizibin <125720240@qq.com>
2018-12-07 11:52:53 +0800

    新增basic和util的common工程

---
+++ black-shop-parent/black-shop-common/black-shop-common-util/src/main/java/cn/blackshop/util/TestUtil.java
@@ -0,0 +1,28 @@
+/**  
+ 
+* <p>Company: www.black-shop.cn</p>  
+
+* <p>Copyright: Copyright (c) 2018</p>   
+
+* black-shop(黑店) 版权所有,并保留所有权利。
+
+*/  
+package cn.blackshop.util;
+
+/**  
+
+* <p>Title: TestUtil</p>  
+
+* <p>Description: </p>  
+
+* @author zibin  
+
+* @date 2018年11月26日  
+
+*/
+public class TestUtil {
+
+	public static void main(String[] args) {
+		System.out.println("测试工具类！！");
+	}
+}
