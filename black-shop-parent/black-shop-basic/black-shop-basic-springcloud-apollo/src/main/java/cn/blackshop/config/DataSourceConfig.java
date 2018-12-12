94fffd96690148e9152ad0914dbae7458693cf1d
e <molost122@163.com>
2018-12-12 22:05:31 +0800

    apollo,springboot,mybatis整合

--- black-shop-parent/black-shop-basic/black-shop-basic-springcloud-apollo/src/main/java/cn/blackshop/config/DataSourceConfig.java
+++ black-shop-parent/black-shop-basic/black-shop-basic-springcloud-apollo/src/main/java/cn/blackshop/config/DataSourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
  * 数据源
  */
 @EnableApolloConfig
-public class DataSourceConfig {
+public abstract class DataSourceConfig {
 
 
     @Value("${driverClassName}")
@@ -26,6 +26,46 @@ public class DataSourceConfig {
     @Value("${connectionProperties}")
     protected String connectionProperties;
 
+    @Value("${datasource_initialSize}")
+    protected Integer initialSize;
 
+    @Value("${datasource_minIdle}")
+    protected Integer minIdle;
+
+    @Value("${datasource_maxActive}")
+    protected Integer maxActive;
+
+    @Value("${datasource_maxWait}")
+    protected Long maxWait;
+
+    @Value("${datasource_timeBetweenEvictionRunsMillis}")
+    protected Long timeBetweenEvictionRunsMillis;
+
+    @Value("${datasource_minEvictableIdleTimeMillis}")
+    protected Long minEvictableIdleTimeMillis;
+
+    @Value("${datasource_validationQuery}")
+    protected String validationQuery;
+
+    @Value("${datasource_testWhileIdle}")
+    protected boolean testWhileIdle;
+
+    @Value("${datasource_testOnBorrow}")
+    protected boolean testOnBorrow;
+
+    @Value("${datasource_testOnReturn}")
+    protected boolean testOnReturn;
+
+    @Value("${datasource_poolPreparedStatements}")
+    protected boolean poolPreparedStatements;
+
+    @Value("${maxPoolPreparedStatementPerConnectionSize}")
+    protected Integer maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${datasource_filters}")
+    protected String filters;
+
+    @Value("${datasource_useGlobalDataSourceStat}")
+    protected boolean useGlobalDataSourceStat;
 
 }
--- black-shop-parent/black-shop-service/black-shop-service-user/black-shop-service-user-basic/src/main/java/cn/blackshop/config/SysUserDataSourceConfig.java
+++ black-shop-parent/black-shop-service/black-shop-service-user/black-shop-service-user-basic/src/main/java/cn/blackshop/config/SysUserDataSourceConfig.java
@@ -4,21 +4,41 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.github.pagehelper.PageHelper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.mapper.autoconfigure.ConfigurationCustomizer;
 
+import java.sql.SQLException;
 import java.util.Properties;
 
 @Configuration
-
+@EnableTransactionManagement
 public class SysUserDataSourceConfig extends DataSourceConfig {
 
     @Bean
-    public DruidDataSource dataSource() {
+    public DruidDataSource dataSource() throws SQLException {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName(driverClassName);
         dataSource.setUrl(userDriverUrl);
         dataSource.setUsername(userName);
         dataSource.setPassword(passWord);
+
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        if (validationQuery != null && !"".equals(validationQuery)) {
+            dataSource.setValidationQuery(validationQuery);
+        }
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        if(poolPreparedStatements){
+            dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        }
+        dataSource.setFilters(filters);
 
         if(connectionProperties != null && !"".equals(connectionProperties)){
             Properties connectProperties = new Properties();
@@ -31,8 +51,14 @@ public class SysUserDataSourceConfig extends DataSourceConfig {
             }
             dataSource.setConnectProperties(connectProperties);
         }
+        dataSource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);
 
         return dataSource;
+    }
+
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager() throws SQLException {//开启事务
+        return new DataSourceTransactionManager(dataSource());
     }
 
     @Bean
@@ -49,6 +75,7 @@ public class SysUserDataSourceConfig extends DataSourceConfig {
 
     @Bean
     public ConfigurationCustomizer configurationCustomizer() {
+
         return new ConfigurationCustomizer() {
             @Override
             public void customize(org.apache.ibatis.session.Configuration configuration) {
