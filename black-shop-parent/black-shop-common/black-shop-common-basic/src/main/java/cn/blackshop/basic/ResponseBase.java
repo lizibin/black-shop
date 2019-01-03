a359022e76e0b6792a8ed0a432b411350cc088ee
lizibin <125720240@qq.com>
2019-01-03 15:56:32 +0800

    修改文件

--- black-shop-parent/black-shop-common/black-shop-common-basic/src/main/java/cn/blackshop/basic/ResponseBase.java
+++ black-shop-parent/black-shop-common/black-shop-common-basic/src/main/java/cn/blackshop/basic/ResponseBase.java
@@ -20,30 +20,6 @@ public class ResponseBase {
 		this.data = data;
 	}
 
-	public Integer getRtnCode() {
-		return rtnCode;
-	}
-
-	public void setRtnCode(Integer rtnCode) {
-		this.rtnCode = rtnCode;
-	}
-
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
 	@Override
 	public String toString() {
 		return "ResponseBase [rtnCode=" + rtnCode + ", msg=" + msg + ", data=" + data + "]";
