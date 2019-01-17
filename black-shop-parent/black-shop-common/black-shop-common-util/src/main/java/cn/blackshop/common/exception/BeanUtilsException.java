292db42b6d66f332976013e73e6a0d7a1e91dd27
lizibin <125720240@qq.com>
2019-01-17 18:22:40 +0800

    新增部分项目

---
+++ black-shop-parent/black-shop-common/black-shop-common-util/src/main/java/cn/blackshop/common/exception/BeanUtilsException.java
@@ -0,0 +1,29 @@
+/**  
+ 
+* <p>Company: www.black-shop.cn</p>  
+
+* <p>Copyright: Copyright (c) 2018</p>   
+
+* @version 1.0  
+
+* black-shop(黑店) 版权所有,并保留所有权利。
+
+*/  
+package cn.blackshop.common.exception;
+
+/**  
+
+* <p>Title: BeanUtilsException</p>  
+
+* <p>Description: </p>  
+
+* @author zibin  
+
+* @date 2019年1月17日  
+
+*/
+public class BeanUtilsException extends RuntimeException{
+	 public BeanUtilsException(Throwable e){
+		    super(e);
+		  }
+}
