39ac674a95698ce6a6cbf3a08d012e8bbec959c8
bestmemory <solememorykai@163.com>
2019-10-13 19:23:36 +0800

    网关服务：修复swagger文档和跨域问题 注释跨域部分代码 添加swagger资源请求 修改网关swagger聚合文档，改用动态获取路由信息生成对应文档

--- black-shop-gateway/src/main/java/cn/blackshop/gateway/config/CorsConfig.java
+++ black-shop-gateway/src/main/java/cn/blackshop/gateway/config/CorsConfig.java
@@ -7,29 +7,25 @@
  */
 package cn.blackshop.gateway.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.http.codec.support.DefaultServerCodecConfigurer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
-
 import reactor.core.publisher.Mono;
 
 /**
  * @author zibin
- *
  */
 @Configuration
 public class CorsConfig {
@@ -46,7 +42,8 @@ public class CorsConfig {
 				HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
 				HttpHeaders headers = response.getHeaders();
 				headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestHeaders.getOrigin());
-				headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, requestHeaders.getAccessControlRequestHeaders());
+				headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+						requestHeaders.getAccessControlRequestHeaders());
 				if (requestMethod != null) {
 					headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
 				}
@@ -68,10 +65,10 @@ public class CorsConfig {
 	 *
 	 * @return the server codec configurer
 	 */
-	@Bean
-	public ServerCodecConfigurer serverCodecConfigurer() {
-		return new DefaultServerCodecConfigurer();
-	}
+//	@Bean
+//	public ServerCodecConfigurer serverCodecConfigurer() {
+//		return new DefaultServerCodecConfigurer();
+//	}
 
 	/**
 	 * 如果使用了注册中心（如：Eureka），进行控制则需要增加如下配置.
@@ -81,8 +78,8 @@ public class CorsConfig {
 	 * @return the route definition locator
 	 */
 	@Bean
-	public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient,
-																		DiscoveryLocatorProperties properties) {
+	public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+			DiscoveryClient discoveryClient,DiscoveryLocatorProperties properties) {
 		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 
