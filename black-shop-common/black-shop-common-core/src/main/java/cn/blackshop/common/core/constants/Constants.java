3f568ef20373689b376ac059a7640d86edd22469
cqiang102 <2375622526@qq.com>
2020-07-02 10:59:29 +0800

    属性使用javadoc方式注释，常量使用大写下划线命名

--- black-shop-common/black-shop-common-core/src/main/java/cn/blackshop/common/core/constants/Constants.java
+++ black-shop-common/black-shop-common-core/src/main/java/cn/blackshop/common/core/constants/Constants.java
@@ -8,37 +8,63 @@
 package cn.blackshop.common.core.constants;
 
 public interface Constants {
-	// 发送邮件
+	/**
+	 * 发送邮件
+	 */
 	String MSG_EMAIL = "email";
-	// 会员token
+	/**
+	 * 会员token
+	 */
 	String TOKEN_MEMBER = "TOKEN_MEMBER";
-	// 用户有效期 90天
+	/**
+	 * 用户有效期 90天
+	 */
 	Long TOKEN_MEMBER_TIME = (long) (60 * 60 * 24 * 90);
-	int COOKIE_TOKEN_MEMBER_TIME = (60 * 60 * 24 * 90);
-	// cookie 会员 totoken 名称
+	Integer COOKIE_TOKEN_MEMBER_TIME = (60 * 60 * 24 * 90);
+	/**
+	 * cookie 会员 token 名称
+	 */
 	String COOKIE_USER_TOKEN = "cookie_user_token";
-	// 微信注册码存放rediskey
+	/**
+	 * 微信注册码存放rediskey
+	 */
 	String WECHAT_CODE_KEY = "weixin.code";
-	// 微信注册码有效期30分钟
+	/**
+	 * 微信注册码有效期30分钟
+	 */
 	Long WECHAT_CODE_TIMEOUT = 30L;
 
-	// 用户信息不存在
+	/**
+	 * 用户信息不存在
+	 */
 	Integer HTTP_RES_CODE_EXISTMOBILE_203 = 203;
 
-	// token
+	/**
+	 * token
+	 */
 	String USER_TOKEN_KEYPREFIX = "mt.mb.login";
 
-	// 安卓的登陆类型
+	/**
+	 * 安卓的登陆类型
+	 */
 	String USER_LOGIN_TYPE_ANDROID = "Android";
-	// IOS的登陆类型
+	/**
+	 * IOS的登陆类型
+	 */
 	String USER_LOGIN_TYPE_IOS = "IOS";
 
-	// PC的登陆类型
+	/**
+	 * PC的登陆类型
+	 */
 	String MEMBER_LOGIN_TYPE_PC = "PC";
 
-	// 登陆超时时间 有效期 90天
-	Long user_LOGIN_TOKEN_TIME = 77776000L;
-	// 用户信息不存在
+	/**
+	 * 登陆超时时间 有效期 90天
+	 */
+	Long USER_LOGIN_TOKEN_TIME = 77776000L;
+	/**
+	 * 用户信息不存在
+	 */
 	Integer HTTP_RES_CODE_NOTUSER_203 = 203;
 
 }
